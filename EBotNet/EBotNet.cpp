8928c54ae33fcb5589d39a09daca238626646c31
playingoDEERUX <62474734+playingoDEERUX@users.noreply.github.com>
2022-09-05 09:19:19 +0200

    fix potential UB

--- EBotNet/EBotNet.cpp
+++ EBotNet/EBotNet.cpp
@@ -32,9 +32,7 @@ void EBotClient::Service()
 		case ENET_EVENT_TYPE_RECEIVE:
 			OnReceived();
 			
-			if (!m_event.packet->userData) // Is always NULL. But sometimes we might just need to quickly send back the same ENetPacket*, then destroying must be ignored here!
-				enet_packet_destroy(m_event.packet);
-
+			enet_packet_destroy(m_event.packet);
 			break;
 
 		case ENET_EVENT_TYPE_DISCONNECT:
